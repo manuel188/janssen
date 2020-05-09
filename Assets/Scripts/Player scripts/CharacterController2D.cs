e0b1a398ab62451f13cb9645fb3cce722a86f705
Mauro <mauro.laraz4182@gmail.com>
2020-05-08 23:23:36 -0700

    Character movement + responsive animations

---
+++ Assets/Scripts/Player scripts/CharacterController2D.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterController2D : MonoBehaviour
+{
+
+}
---
+++ Assets/Scripts/Player scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+
+}
